dae762b7027cfeef9918c369cd439ca4eff4f191
Quentin Raquet <quentin.raquet@gmail.com>
2024-05-07 01:42:23 +0200

    add content and update layout

--- main.js
+++ main.js
@@ -13,13 +13,45 @@ document.querySelectorAll('.nav-link').forEach(link => {
     });
 });
 
-window.addEventListener("scroll", function () {
-    const windowScrollTop = window.scrollTop;
-    const elementToHide = document.getElementById("main-content");
 
-    elementToHide.style.clipPath = `inset(${windowScrollTop}px 0 0 0)`;
-});
+function applyBounceEffect() {
+    const mainContent = document.getElementById('main-content');
+    const scrollTop = mainContent.scrollTop;
+    const scrollHeight = mainContent.scrollHeight;
+    const clientHeight = mainContent.clientHeight;
+    const threshold = 5; // Adjust this value as needed
 
+    if (scrollTop === 0) {
+        mainContent.style.animation = 'bounce_top 0.5s';
+    } else if (scrollHeight - scrollTop <= clientHeight + threshold) {
+        mainContent.style.animation = 'bounce_bottom 0.5s';
+    } else {
+        mainContent.style.animation = '';
+    }
+}
+
+function applyAppearenceEffect() {
+    const sections = document.querySelectorAll('.section');
+    const windowHeight = window.innerHeight;
+
+    sections.forEach(section => {
+        const distanceFromTop = section.getBoundingClientRect().top;
+        if (distanceFromTop <= windowHeight / 2) {
+            section.style.opacity = '1';
+        } else {
+            section.style.opacity = '0.1';
+        }
+    });
+}
+
+function animatedSkillsBar() {
+    let bars = document.querySelectorAll('.skills-bar .bar');
+    bars.forEach(bar => {
+        let skillsBars = bar.parentElement;
+        let percent = skillsBars.parentElement.getAttribute('data-percent');
+        bar.style.width = percent + '%';
+    });
+}
 
 function loadPage(url) {
     fetch(url)
@@ -29,8 +61,13 @@ function loadPage(url) {
             mainContent.innerHTML = data;
             // Scroll main-content to top after new content is loaded
             mainContent.scrollTop = 0;
+            mainContent.addEventListener('scroll', applyBounceEffect);
+            mainContent.addEventListener('scroll', applyAppearenceEffect);
+            animatedSkillsBar();
         });
 }
+
+
 
 // Load the home page by default
 loadPage('home.html');
